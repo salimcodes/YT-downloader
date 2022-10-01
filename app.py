a124d85f0859ae322cc0a07516221cd412a8b77b
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2022-10-01 14:26:33 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+from pytube import YouTube
+import os
+
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+    yt = request.form['text']
+    yt = YouTube(yt)
+    video = yt.streams.filter(only_audio = True).first()
+    destination = '.'
+    out_file = video.download(output_path = destination)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    return render_template('results.html')
